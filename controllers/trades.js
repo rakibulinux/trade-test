91fd85b6a4ba0baf94f3c326831bfbe61e470395
Admin <admin.push@hackerrank.workspace>
2023-04-04 18:25:07 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- controllers/trades.js
+++ controllers/trades.js
@@ -47,19 +47,12 @@ module.exports.getSpecificTrade = async (req, res) => {
 
 module.exports.updateSpecificTrade = async (req, res) => {
   try {
-    const [numUpdated, updatedTrade] = await Trade.update(req.body, {
-      where: { id: req.params.id },
-      returning: true,
-    });
-    if (numUpdated) {
-      res.json(updatedTrade[0]);
-    } else {
-      res.status(405).send('Trade not found'); // Set the status code to 405
-    }
+    res.status(405).send('Deleting or modifying trades is not allowed');
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
+
 
 
 module.exports.deleteSpecificTrade = async (req, res) => {
