df1ce58f02fced3f68ed3601e1c562ae7a052b5e
Admin <admin.push@hackerrank.workspace>
2023-04-04 18:30:21 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- controllers/trades.js
+++ controllers/trades.js
@@ -31,39 +31,75 @@ try {
 };
 
 
+// module.exports.getSpecificTrade = async (req, res) => {
+//   try {
+//     const trade = await Trade.findByPk(req.params.id);
+//     if (trade) {
+//       res.json(trade);
+//     } else {
+//       res.status(404).send('Trade not found'); // Set the status code to 400
+//     }
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+
+// module.exports.updateSpecificTrade = async (req, res) => {
+//   try {
+//     const [numUpdated, updatedTrade] = await Trade.update(req.body, {
+//       where: { id: req.params.id },
+//       returning: true,
+//     });
+//     if (numUpdated) {
+//       res.json(updatedTrade[0]);
+//     } else {
+//       res.status(405).send('Trade not found'); // Set the status code to 405
+//     }
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+
+// module.exports.deleteSpecificTrade = async (req, res) => {
+//   try {
+//     const numDeleted = await Trade.destroy({
+//       where: { id: req.params.id },
+//     });
+//     if (numDeleted) {
+//       res.sendStatus(405);
+//     } else {
+//       res.status(400).send('Trade not found'); // Set the status code to 400
+//     }
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// };
+
+module.exports.updateSpecificTrade = async (req, res) => {
+  try {
+    res.status(405).send('Method Not Allowed'); // Set the status code to 405
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports.deleteSpecificTrade = async (req, res) => {
+  try {
+    res.status(405).send('Method Not Allowed'); // Set the status code to 405
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 module.exports.getSpecificTrade = async (req, res) => {
   try {
     const trade = await Trade.findByPk(req.params.id);
     if (trade) {
       res.json(trade);
     } else {
-      res.status(404).send('Trade not found'); // Set the status code to 400
-    }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
-
-
-module.exports.updateSpecificTrade = async (req, res) => {
-  try {
-    res.status(405).send('Deleting or modifying trades is not allowed');
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
-
-
-
-module.exports.deleteSpecificTrade = async (req, res) => {
-  try {
-    const numDeleted = await Trade.destroy({
-      where: { id: req.params.id },
-    });
-    if (numDeleted) {
-      res.sendStatus(405);
-    } else {
-      res.status(400).send('Trade not found'); // Set the status code to 400
+      res.status(404).send('ID not found'); // Set the status code to 404
     }
   } catch (error) {
     res.status(500).send(error.message);
